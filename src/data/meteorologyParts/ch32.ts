693340ca63ba7210dbff6c322e6935990c0f2c43
gerdjan <christiaanwapenaar@hotmail.com>
2026-09-07 22:44:01 +0200

    Assemble Meteorology chapter 3.2

---
+++ src/data/meteorologyParts/ch32.ts
@@ -0,0 +1,6 @@
+import type { SourcedUnit } from '../../domain/types'
+import { meteorology32Part1 } from './ch32part1'
+import { meteorology32Part2 } from './ch32part2'
+import { meteorology32Part3 } from './ch32part3'
+
+export const meteorology32Units:SourcedUnit[]=[...meteorology32Part1,...meteorology32Part2,...meteorology32Part3]
