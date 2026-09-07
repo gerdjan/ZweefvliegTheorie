8a13a7a5be9ff326ac22284059013cd0f30d1bf5
gerdjan <christiaanwapenaar@hotmail.com>
2026-09-07 17:10:12 +0200

    Mark Principles 5.3 detail-audited

--- src/data/principlesCourse.ts
+++ src/data/principlesCourse.ts
@@ -8,6 +8,7 @@ import { units as completenessB } from './principlesParts/aeroCompletenessB'
 import { units as mechanics } from './principlesParts/mechanics'
 import { units as mechanicsCompleteness } from './principlesParts/mechanicsCompleteness'
 import { units as stabilityControls } from './principlesParts/stabilityControls'
+import { units as stabilityCompleteness } from './principlesParts/stabilityCompleteness'
 import { units as limits } from './principlesParts/limits'
 import { units as safetyFinal } from './principlesParts/safetyFinal'
 
@@ -15,7 +16,7 @@ export const principlesSubject:SourcedSubject = {
   id:'principles',
   number:5,
   title:'Beginselen van het zweefvliegen',
-  sourceFreshness:'35 levels · theorie november 2025 · detailaudit 78,6%',
-  description:'35 levels over aerodynamica, vliegmechanica, stabiliteit, besturing, belastingen, overtrek, tolvlucht en spiraalduik. Elk theorieblok en elke vraag heeft een controleerbare PDF-bron. 5.0, 5.1 en 5.2 zijn nu pagina-voor-pagina op detailniveau gecontroleerd; de overige hoofdstukken volgen.',
-  units:[...basics,...completenessA,...aeroA,...density,...aeroB,...completenessB,...mechanics,...mechanicsCompleteness,...stabilityControls,...limits,...safetyFinal],
+  sourceFreshness:'38 levels · theorie november 2025 · detailaudit 81,6%',
+  description:'38 levels over aerodynamica, vliegmechanica, stabiliteit, besturing, belastingen, overtrek, tolvlucht en spiraalduik. Elk theorieblok en elke vraag heeft een controleerbare PDF-bron. 5.0 t/m 5.3 zijn nu pagina-voor-pagina op detailniveau gecontroleerd; 5.4 t/m 5.7 volgen.',
+  units:[...basics,...completenessA,...aeroA,...density,...aeroB,...completenessB,...mechanics,...mechanicsCompleteness,...stabilityControls,...stabilityCompleteness,...limits,...safetyFinal],
 }
