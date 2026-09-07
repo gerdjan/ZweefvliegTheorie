72b4a5d7cb1790df99d4828fb67e79658ccada2f
gerdjan <christiaanwapenaar@hotmail.com>
2026-09-07 17:32:51 +0200

    Mark Principles 5.4 detail audit complete

--- src/data/principlesCourse.ts
+++ src/data/principlesCourse.ts
@@ -9,6 +9,7 @@ import { units as mechanics } from './principlesParts/mechanics'
 import { units as mechanicsCompleteness } from './principlesParts/mechanicsCompleteness'
 import { units as stabilityControls } from './principlesParts/stabilityControls'
 import { units as stabilityCompleteness } from './principlesParts/stabilityCompleteness'
+import { units as controlsCompleteness } from './principlesParts/controlsCompleteness'
 import { units as limits } from './principlesParts/limits'
 import { units as safetyFinal } from './principlesParts/safetyFinal'
 
@@ -16,7 +17,7 @@ export const principlesSubject:SourcedSubject = {
   id:'principles',
   number:5,
   title:'Beginselen van het zweefvliegen',
-  sourceFreshness:'38 levels · theorie november 2025 · detailaudit 81,6%',
-  description:'38 levels over aerodynamica, vliegmechanica, stabiliteit, besturing, belastingen, overtrek, tolvlucht en spiraalduik. Elk theorieblok en elke vraag heeft een controleerbare PDF-bron. 5.0 t/m 5.3 zijn nu pagina-voor-pagina op detailniveau gecontroleerd; 5.4 t/m 5.7 volgen.',
-  units:[...basics,...completenessA,...aeroA,...density,...aeroB,...completenessB,...mechanics,...mechanicsCompleteness,...stabilityControls,...stabilityCompleteness,...limits,...safetyFinal],
+  sourceFreshness:'40 levels · theorie november 2025 · detailaudit 83,3%',
+  description:'40 levels over aerodynamica, vliegmechanica, stabiliteit, besturing, belastingen, overtrek, tolvlucht en spiraalduik. Elk theorieblok en elke vraag heeft een controleerbare PDF-bron. 5.0 t/m 5.4 zijn nu pagina-voor-pagina op detailniveau gecontroleerd; 5.5 t/m 5.7 volgen.',
+  units:[...basics,...completenessA,...aeroA,...density,...aeroB,...completenessB,...mechanics,...mechanicsCompleteness,...stabilityControls,...stabilityCompleteness,...controlsCompleteness,...limits,...safetyFinal],
 }
