143f9d6f9edbd657fea84072e9413f61b6d60bd7
gerdjan <christiaanwapenaar@hotmail.com>
2026-09-08 05:16:47 +0200

    Use full Meteorology course in curriculum

--- src/data/curriculum-full.ts
+++ src/data/curriculum-full.ts
@@ -1,6 +1,9 @@
 import { subjects as baseSubjects } from './curriculum'
+import { meteorologySubject } from './meteorologyCourse'
 import { principlesSubject } from './principlesCourse'
 
-export const subjects = baseSubjects.map(subject =>
-  subject.id === 'principles' ? principlesSubject : subject
-)
+export const subjects = baseSubjects.map(subject => {
+  if(subject.id === 'meteo') return meteorologySubject
+  if(subject.id === 'principles') return principlesSubject
+  return subject
+})
