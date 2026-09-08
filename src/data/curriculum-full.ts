21950682d73a7cbaf1a8e9baaf8020f2d929e97b
gerdjan <christiaanwapenaar@hotmail.com>
2026-09-08 08:26:59 +0200

    Wire Human Performance into curriculum

--- src/data/curriculum-full.ts
+++ src/data/curriculum-full.ts
@@ -1,8 +1,10 @@
 import { subjects as baseSubjects } from './curriculum'
+import { humanSubject } from './humanCourse'
 import { meteorologySubject } from './meteorologyCourse'
 import { principlesSubject } from './principlesCourse'
 
 export const subjects = baseSubjects.map(subject => {
+  if(subject.id === 'human') return humanSubject
   if(subject.id === 'meteo') return meteorologySubject
   if(subject.id === 'principles') return principlesSubject
   return subject
